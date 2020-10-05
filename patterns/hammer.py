a5ecf334f8a2263c60d0cf5a54328cc201645870
Idanq <7949659+idanq1@users.noreply.github.com>
2020-10-05 13:57:05 +0300

    Hammer recognition done.

--- patterns/hammer.py
+++ patterns/hammer.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import datetime
 from patterns.CandleClass import Candle
+import time
+s = time.time()
 
 
 def get_candle_class(ticker, period="max", interval="1d", start=None, end=None):
@@ -12,7 +14,7 @@ def get_candle_class(ticker, period="max", interval="1d", start=None, end=None):
 
     candles = []
 
-    data = yf.download(ticker, period=period, interval=interval, start=start, end=end)
+    data = yf.download(ticker, period=period, interval=interval, start=start, end=end, progress=False, show_errors=False)
     rows = data.shape[0]
     for i in range(rows):
         open_ = data[["Open"]]["Open"].iloc[i]
@@ -22,13 +24,56 @@ def get_candle_class(ticker, period="max", interval="1d", start=None, end=None):
         vol = data[["Volume"]]["Volume"].iloc[i]
         date = data.index[i]
         candles.append(Candle(ticker, open_, close, high, low, vol, date))
-    return reversed(candles)
+    # return reversed(candles)
+    return candles
 
 
-# TODO: Find trend
-# TODO: Do for hammer and hanging well as well
+def get_tickers():
+    tickers = []
+    with open(r"..\nasdaqtraded.txt", 'r') as f:
+        stock_list = f.readlines()
 
-tsla_candles = get_candle_class("TSLA", "max", "1d")
-for candle in tsla_candles:
-    if candle.head() >= candle.body() * 2 and candle.tail() <= candle.body() * 1:  # Inverted hammer, shooting star
-        print(candle.date)
+    for stock in stock_list:  # 700
+        stock_ticker = stock.split("|")[1]
+        if stock_ticker == "Symbol":
+            continue
+        if "$" in stock_ticker:
+            continue
+        if stock_ticker == "":
+            continue
+        tickers.append(stock_ticker)
+    return tickers
+
+
+tickers = get_tickers()
+
+for ticker in tickers:
+    ticker_candles = get_candle_class(ticker, "10d", "1d")
+    sma_period = 9
+    sma_calc = []
+    sma_value = None
+    for candle in ticker_candles:
+        if len(sma_calc) < sma_period:
+            sma_calc.append(candle.close)
+        else:
+            sma_value = sum(sma_calc) / sma_period
+            sma_calc = sma_calc[1:]
+            sma_calc.append(candle.close)
+
+        if sma_value:
+            if candle.head() >= candle.body() * 2 and candle.tail() <= candle.body() * 0.8:  # Inverted hammer & Shooting star
+                if sma_value < candle.close:  # Downtrend
+                    print(f"Bullish- {ticker}- Inverted Hammer- {candle.date}")
+                else:  # Uptrend
+                    print(f"Dubi- {ticker}- Shooting star- {candle.date}")
+            elif candle.tail() >= candle.body() * 2 and candle.head() <= candle.body() * 0.8:  # Hammer & Hanging man
+                if sma_value < candle.close:  # Downtrend
+                    print(f"Bullish- {ticker}- Hammer- {candle.date}")
+                else:  # Uptrend
+                    print(f"Dubi- {ticker}- Hanging man- {candle.date}")
+        # else:
+        #     if candle.head() >= candle.body() * 2 and candle.tail() <= candle.body() * 0.8:  # Shooting Star
+        #         print(f"Bullish- Shooting Star- {candle.date}")
+        #     elif candle.tail() >= candle.body() * 2 and candle.head() <= candle.body() * 0.8:  # Hammer
+        #         print(f"Bullish- - {candle.date}")
+                # if candle.head() >= candle.body() * 2 and candle.tail() <= candle.body() * 1:  # Inverted hammer, shooting star
