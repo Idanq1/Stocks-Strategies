2497e276aa56b718111831e7c63ab5a82aa5a5da
Idanq <7949659+idanq1@users.noreply.github.com>
2020-10-07 00:44:50 +0300

    Morning star finished (I hope)
    Added Evening star, Volume threshold, SMA (14) for trend

--- patterns/morning star.py
+++ patterns/morning star.py
@@ -2,14 +2,11 @@ import yfinance as yf
 import datetime
 from patterns.CandleClass import Candle
 import time
-import pandas as pd
 
 s = time.time()
 
 # TODO: Better doji
 # TODO: Better morning star
-# TODO: Evening star
-# TODO: Look for trend
 
 
 def download_candles(ticker, period="max", interval="1d", start=None, end=None):
@@ -66,23 +63,32 @@ volume_threshold = 100000  # Filter out stocks with less than this volume.
 tmp_tickers = []
 ticker_candles = None
 for stock_ticker in tickers:
-    # stock_ticker = "CEL"
-    if n < div:
+    if n < div < len(tickers):  # Last part because if I'm using less than 700 list
         tmp_tickers.append(stock_ticker)
         n += 1
         continue
     n = 0
-    # ticker_candles = get_candle_class(ticker, "10d", "1d")
-    # tmp_tickers = ["CEL"]
-    candles = download_candles(tmp_tickers, "3d", "1d")
+    candles = download_candles(tmp_tickers, "15d", "1d")
     for tmp_ticker in tmp_tickers:
-        # -------------- Ticker ------------
-        # tmp_ticker = ["CEL"]
+        # -------------- Ticker --------------
         ticker_candles = get_candles(candles[tmp_ticker], tmp_ticker)
+        if ticker_candles[-2].volume < volume_threshold:  # Two because the volume on the last candle isn't complete
+            continue
         first_candle = None
         second_candle = None
+        sma_period = 14
+        sma_calc = []
+        sma_value = None
         # F - S - C
         for candle in ticker_candles:
+            # --------------- Candle (day) ---------------
+            if len(sma_calc) < sma_period:  # SMA calculation
+                sma_calc.append(candle.close)
+            else:
+                sma_value = sum(sma_calc) / sma_period
+                sma_calc = sma_calc[1:]
+                sma_calc.append(candle.close)
+
             if not second_candle:
                 second_candle = candle
                 continue
@@ -90,9 +96,16 @@ for stock_ticker in tickers:
                 first_candle = second_candle
                 second_candle = candle
                 continue
-            if not first_candle.is_green() and second_candle.is_doji():
-                if first_candle.close < candle.close < first_candle.open and candle.is_green():
-                    print(f"{candle.ticker}- {candle.date}")
+
+            if sma_value:
+                if sma_value > first_candle.close:  # Downtrend
+                    if not first_candle.is_green() and second_candle.is_doji():  # and second_candle.close < first_candle.close:  # Morning star
+                        if first_candle.close < candle.close < first_candle.open and candle.is_green():
+                            print(f"Morning star- {candle.ticker}- {candle.volume}")
+                else:  # Uptrend
+                    if first_candle.is_green() and second_candle.is_doji():  # Evening star
+                        if first_candle.open < candle.close < first_candle.close:
+                            print(f"Evening star- {candle.ticker}- {candle.volume}")
             first_candle = second_candle
             second_candle = candle
     tmp_tickers = []
